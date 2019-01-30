978fcedfa02d047e9a916142209aedc144b7150e
Roumit <timur.belotserkovsky@gmail.com>
2019-01-30 17:54:43 +0200

    Add files via upload

--- PVPower.py
+++ PVPower.py
@@ -17,15 +17,26 @@ from pvlib.location import Location
 
 tus = Location(30.5, 50, 'Etc/GMT-3', 300, 'Kiev')
 
-Times = pd.date_range(start='2017-07-01', end='2017-07-02', freq='1min', tz=tus.tz) # DatetimeIndex(start='2017-07-01', end='2017-07-02', freq='1min', tz=tus.tz)
+Times = pd.date_range(start='2017-12-01', end='2017-12-02', freq='1min', tz=tus.tz) # DatetimeIndex(start='2017-07-01', end='2017-07-02', freq='1min', tz=tus.tz)
 # print(Times)
 
 cs = tus.get_clearsky(Times)  # ineichen with climatology table by default
 
-# print(cs)
+# print(type(cs["ghi"].at['2017-12-01 00:09:00+03:00']))
 
-cs.plot()
+
+
+cs['ghi'].plot()
+plt.contour
 plt.ylabel('Irradiance $W/m^2$')
 plt.title('Ineichen, climatological turbidity')
 
-plt.show(block = True)
+# plt.show(block = True)
+
+file_csv = open("day-clear.csv" , 'w')
+for i in cs['ghi']:
+    file_csv.write(str(i))
+    file_csv.write("\n")
+file_csv.close()
+
+
